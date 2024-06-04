2713b6b2fbc10f3be44eb5672aaf9d9a5480e9e9
feeroza <160626229+feeroza@users.noreply.github.com>
2024-06-04 23:28:29 +0500

    Add files via upload
    this is my first repository

---
+++ task 1/variable.ts
@@ -0,0 +1,94 @@
+
+//variable Declaration
+
+let name1: string = "John Doe";
+let age: number = 30;
+
+console.log(name1);
+console.log(age);
+
+
+let firstName: string = "john";
+let lastName: string = "Doe";
+firstName  = "John Doe";
+console.log(firstName);
+
+
+const isAdmin: boolean = true
+console.log(isAdmin);
+
+
+
+
+
+//concatination
+
+let firstName1: string = "john";
+let lastName1: string = "Doe";
+
+console.log("fullName " + firstName1+ " " +lastName1);
+
+
+
+let greeting: string = "Hello!";
+let name1: string = "john Doe";
+
+console.log(greeting + " " +name 1);
+
+
+
+
+
+//primitive Data type
+
+const num : number = 10
+const str : string = "string"
+const bool : boolean = true
+
+console.log(num);
+console.log(str);
+console.log(bool);
+
+
+
+
+
+
+
+
+
+//Data type
+// Object
+
+const person= {
+    name: "john Doe",
+    age: 30
+}
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+
+
+let adress = {
+    street: "123 Main St",
+    city: "Anytown",
+    state: "CA"
+}
+
+console.log(adress);
+console.log(adress.street);
+console.log(adress.state);
+
+
+
+
+
+//array 
+
+let colours = ["red","green","blue"]
+console.log(colours);
+
+console.log(colours[2])
+
+console.log(colours[0]);
+
